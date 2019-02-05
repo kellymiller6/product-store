9bec51caac7238b3e3cf8401aea27e845599c27e
kelly miller <kelly@choozle.com>
2019-02-05 13:19:36 -0700

    paginator component working items not responding

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import styled from 'styled-components';
-import Head from 'next/head';
+import styled from "styled-components";
+import Head from "next/head";
 import Error from "./ErrorMessage";
 
 const SingleItemStyles = styled.div`
-    max-width: 1200px;
-    margin: 2rem auto;
-    box-shadow: ${props => props.theme.bs};
-    display: grid;
-    grid-auto-columns: 1fr;
-    grid-auto-flow: column;
-    min-height: 800px;
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: contain;
-    }
-    .details {
-        margin: 3rem;
-        font-size: 2rem;
-    }
-`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
+`;
 const SINGLE_ITEM_QUERY = gql`
   query SINGLE_ITEM_QUERY($id: ID!) {
     item(where: { id: $id }) {
@@ -46,22 +46,21 @@ class SingleItem extends Component {
         {({ error, loading, data }) => {
           if (error) return <Error error={error} />;
           if (loading) return <p>Loading</p>;
-          if (!data.item) return <p>No item found for {this.props.id}</p>
+          if (!data.item) return <p>No item found for {this.props.id}</p>;
           const item = data.item;
-          console.log(item)
+          console.log(item);
           return (
             <SingleItemStyles>
-                <Head>
-                    <title>Product Store | {item.title}</title>
-                </Head>
-                 <img src={item.largeImage} alt={item.title} /> 
-                 <div className="details">
-                    <h2>Viewing {item.title}</h2>
-                    <p>{item.description}</p>
-                 </div>
-
+              <Head>
+                <title>Product Store | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
             </SingleItemStyles>
-          )
+          );
         }}
       </Query>
     );
