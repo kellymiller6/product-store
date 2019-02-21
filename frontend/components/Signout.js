46d1a19dec872ea3a7898ad034d820733a45b541
kelly miller <kelly@choozle.com>
2019-02-21 08:42:36 -0700

    signout component in nav

---
+++ frontend/components/Signout.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
+
+const Signout = props => (
+<button>Signout</button>
+)
+
+export default Signout;
